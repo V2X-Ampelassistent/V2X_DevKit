c5502b0f0d840615887db013ed4c4af423142b4f
SchulzJonathan <js.skype@online.de>
2024-11-07 17:15:17 +0100

    Mostly working except for ComputedLane.msg

--- ASN1toROS2/run.bash
+++ ASN1toROS2/run.bash
@@ -4,7 +4,7 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-rm msg/*.msg -y
+rm msg/*.msg
 # make shure to import ETSI-ITS-CDD last, as it might have duplicate messages that will be ignored this way.
 ./main.py rsc/ETSI/DRSC.asn rsc/ETSI/MAPEM-PDU-Descriptions.asn rsc/ETSI/SPATEM-PDU-Descriptions.asn rsc/ETSI/ETSI-ITS-CDD.asn
 # ./main.py test.asn
